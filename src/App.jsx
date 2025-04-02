400608e1f2b217b15f49dcd7b6593c607d57c8b7
Dan Finley <44242241+dannyjive@users.noreply.github.com>
2025-04-01 20:00:11 -0700

    added name change useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
 
 function App() {
   return (
     <>
       <Player />
+
       <div id="challenges"></div>
     </>
   );
